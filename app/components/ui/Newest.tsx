aad19c74c2a81c81cf7975880dea32b4cc7d6c72
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-03-27 15:47:29 +0400

    fix/add newest article image wrapper

--- app/components/ui/Newest.tsx
+++ app/components/ui/Newest.tsx
@@ -12,30 +12,32 @@ export default function Newest({ newest }: { newest: ArticleType }) {
 
       <Link href={`/articles/${newest.slug}`}>
         <div className="flex flex-col items-start bg-white dark:bg-slate-600 shadow-lg hover:shadow-xl rounded-lg max-w-3xl h-full overflow-hidden transition-shadow">
-          {newest.imageUrl !== null ? (
-            <Image
-              className="w-full object-cover"
-              src={
-                newest.imageUrl.endsWith(".jpg")
-                  ? newest.imageUrl
-                  : newest.imageUrl + ".jpeg"
-              }
-              alt={newest.title}
-              width={1000}
-              height={1000}
-              loading="eager"
-            />
-          ) : (
-            <Image
-              className="w-full object-cover"
-              src="/no-image-available-icon-vector.jpg"
-              alt="No Image"
-            />
-          )}
+          <div className="w-full h-90">
+            {newest.imageUrl !== null ? (
+              <Image
+                className="w-full object-cover h-full"
+                src={
+                  newest.imageUrl.endsWith(".jpg")
+                    ? newest.imageUrl
+                    : newest.imageUrl + ".jpeg"
+                }
+                alt={newest.title}
+                width={1000}
+                height={1000}
+                loading="eager"
+              />
+            ) : (
+              <Image
+                className="w-full object-cover"
+                src="/no-image-available-icon-vector.jpg"
+                alt="No Image"
+              />
+            )}
+          </div>
 
           <div className="flex flex-col gap-4 p-6 pt-2">
             <h2 className="text-3xl">{newest.title}</h2>
-            <p>{newest.description}</p>
+            <p>{newest.description.slice(0, 200) + "..."}</p>
             <p>{formatedDate}</p>
           </div>
         </div>
