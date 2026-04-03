5b5c46dd0025b8467b74018e97044a424daf5ace
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-04-03 14:30:29 +0400

    hot-fix/changed path name to category b&t

--- app/constants/links.ts
+++ app/constants/links.ts
@@ -2,6 +2,6 @@ export const links = [
   { name: "სიახლეები", path: "/news" },
   { name: "გასტრონომია", path: "/gastronomy" },
   { name: "სასტუმროები", path: "/hotels" },
-  { name: "ბიზნესი და ტექნოლოგია", path: "/business_&_tech" },
+  { name: "ბიზნესი და ტექნოლოგია", path: "/business-and-tech" },
   { name: "ინტერვიუები", path: "/interviews" },
 ];
