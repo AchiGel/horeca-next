d9dc6ae76eb799e1558f25319964647f99957a9a
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-04-03 18:38:31 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.ibb.co",
-      },
-      {
-        protocol: "https",
-        hostname: "imgur.com",
-      },
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "imgur.com" },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/og-logo.png",
+        headers: [
+          { key: "Content-Type", value: "image/png" },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
