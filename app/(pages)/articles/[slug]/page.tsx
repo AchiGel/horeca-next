6a61a60bf134414559b1abbd25da860104f7af9a
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-05-21 14:33:06 +0400

    Update page.tsx

--- app/(pages)/articles/[slug]/page.tsx
+++ app/(pages)/articles/[slug]/page.tsx
@@ -81,7 +81,7 @@ export default async function Page({ params }: Props) {
                   <SafeImage
                     alt={article.slug}
                     src={article.imageUrl}
-                    className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                    className="w-full h-full object-contain transition-transform duration-300 group-hover:scale-105"
                   />
                 </div>
                 <h3 className="text-base font-medium leading-snug group-hover:underline line-clamp-2">
